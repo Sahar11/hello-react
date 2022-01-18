0b4dbf3301e03ad02f9edd3d7c037a91e084ff71
Sahar11 <saharhalim151@gmail.com>
2022-01-18 15:34:38 -0500

    add react components

--- src/index.js
+++ src/index.js
@@ -3,22 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 const Button = (props) => {
-  // your code here
+  return (
+    <button onClick={props.reset}> {props.text} </button>
+  );
 };
 
 const Application = () => {
 
-  // your code here
-
+  const [name, setName] = useState('');
+  const handleInput = (event) => setName(event.target.value);
   const reset = () => {
-    console.log("reset");
-    // your code here
+    setName('');
   };
 
   return (
     <main>
-      {/* your code here -- this entire line including the curly braces can be removed */}
-      <h1>Hello React</h1>
+      <input placeholder='Type your name' value={name} onChange={handleInput} />
+      <Button text="reset" reset={reset} />
+      {name && <h1>Hello {name}</h1>}
+      {!name && ""}
     </main>
   );
 };
